938a2c9896d5a46f6255e6bb798d333e4ebfbd9e
student <nasulina2015@yandex.ru>
2023-02-16 14:37:33 +0300

    laba

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-mesto = input('Введите номер места')
-
+mesto = int(input('Введите номер места'))
+if mesto % 2 == 0 and mesto <= 36:
+    print('Верхнее место в купе')
+elif mesto % 2 != 0 and mesto <= 35:
+    print('Нижнее место в купе')
+elif mesto % 2 == 0 and mesto > 36:
+    print('Верхнее боковое место')
+else:
+    print('Нижнее боковое место')
